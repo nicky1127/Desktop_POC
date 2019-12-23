3250bc74e74f561b1a15171e22c869fe5383bf13
9503664 <a9503664@9503664s-mbp.home>
2019-12-23 12:51:48 +0000

    add appbar in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Grid from '@material-ui/core/Grid';
-import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
@@ -15,24 +16,31 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import LockIcon from '@material-ui/icons/Lock';
 import TextField from '@material-ui/core/TextField';
-import MainHeader from '../components/MainHeader';
-import WorkingContainer from '../components/WorkingContainer';
-import AvayaToolBar from '../components/AvayaToolBar';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    flexGrow: 1,
+    backgroundColor: '#eceff1',
+    height: '100vh'
+  },
+  appBar: {
+    backgroundColor: '#00695c'
+  },
+  container: {
+    paddingTop: '100px'
+  },
+  appTitle: {
     flexGrow: 1
   },
-  paper: {
-    height: '23vh',
-    backgroundColor: '#9e9e9e'
+  menuButton: {
+    marginRight: theme.spacing(2)
   },
   title: {
     padding: '20px 0'
   },
   formWrapper: {
     padding: '30px',
-    marginTop: '100px',
+    backgroundColor: '#fff',
     border: '1px solid #bdbdbd',
     borderRadius: '5px'
   },
@@ -44,8 +52,8 @@ const useStyles = makeStyles(theme => ({
     margin: '20px 0'
   },
   forgotPW: {
-      fontSize: '12px',
-      lineHeight:'25px'
+    fontSize: '12px',
+    lineHeight: '25px'
   }
 }));
 
@@ -69,8 +77,19 @@ function Main() {
     }
   };
   return (
-    <div>
-      <Container maxWidth="sm">
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.appTitle}>
+            NGCC Desktop
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm" className={classes.container}>
         <Box className={classes.formWrapper}>
           <Typography className={classes.title} variant="h5">
             Log in
