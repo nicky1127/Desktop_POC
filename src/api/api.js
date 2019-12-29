a4c9174b3b8cd3e1c57479e24453d06e76399e6c
9503664 <a9503664@9503664s-mbp.home>
2019-12-29 19:31:57 +0000

    add axios library and authLogin api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,24 @@
+import axios from 'axios';
+
+const baseURL = '/api';
+const uriAuth = '/auth';
+const config = { baseURL };
+const http = axios.create(config);
 
 export class Api {
+  async authLogin({ username, password }) {
+    const auth = { username, password };
+    let response;
 
+    try {
+      const httpResponse = await http.post(uriAuth, {}, { auth });
+      response = httpResponse.data;
+    } catch (err) {
+      console.log('err', err);
+    }
+
+    return response;
+  }
 }
 
 const api = new Api();
