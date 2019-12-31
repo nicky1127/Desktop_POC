1d11b2609c4866ad0ddb657c4b91d3d74c413f92
9503664 <a9503664@9503664s-mbp.home>
2019-12-31 22:40:42 +0000

    add expansion dropdown content with select components

--- src/components/Expansion.js
+++ src/components/Expansion.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import moment from 'moment';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Collapse from '@material-ui/core/Collapse';
+import Fade from '@material-ui/core/Fade';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
@@ -16,6 +17,16 @@ import Divider from '@material-ui/core/Divider';
 import HomeIcon from '@material-ui/icons/Home';
 import CakeIcon from '@material-ui/icons/Cake';
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+
+//carousel
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
 import constants from '../constants';
 import callInfo from '../mock/api/callInfo.json';
@@ -36,13 +47,13 @@ const useStyles = makeStyles(theme => ({
   },
   name: {
     fontWeight: 'bold',
-    overflow:'hidden',
+    overflow: 'hidden',
     width: '60%',
     height: '20px'
   },
   address: {
     fontWeight: 'bold',
-    overflow:'hidden',
+    overflow: 'hidden',
     width: '60%',
     height: '20px'
   },
@@ -63,8 +74,20 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     bottom: '5px',
     left: '49%',
-    transition:'transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    transition: 'transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
     transform: props => (props.checked ? 'rotate(180deg)' : 'rotate(0deg)')
+  },
+  expansionDropdownContent: {
+    margin: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    height: '20vh'
+  },
+  stepper: {
+    flexGrow: 1
+  },
+  formControl: {
+    margin: theme.spacing(2),
+    minWidth: 120
   }
 }));
 
@@ -75,6 +98,50 @@ export default function Expansion(props) {
   const onClickBtn = () => {
     setChecked(prev => !prev);
   };
+
+  //carousel
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [bank, setBank] = React.useState('');
+  const [account, setAccount] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => {
+      if (prevActiveStep === 1) {
+        return 0;
+      } else {
+        return prevActiveStep + 1;
+      }
+    });
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => {
+      if (prevActiveStep === 0) {
+        return 1;
+      } else {
+        return prevActiveStep - 1;
+      }
+    });
+  };
+
+  const handleChangeBank = event => {
+    setBank(event.target.value);
+  };
+
+  const handleChangeAccount = event => {
+    setAccount(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  //
 
   return (
     <div className={classes.root}>
@@ -88,7 +155,7 @@ export default function Expansion(props) {
                     <PersonIcon />
                   </ListItemIcon>
                   <ListItemText
-                    classes={{root:classes.name}}
+                    classes={{ root: classes.name }}
                     primary={`${customers.title}  ${customers.name}`}
                   />
                 </ListItem>
@@ -106,7 +173,7 @@ export default function Expansion(props) {
               <ListItemIcon className={classes.icon}>
                 <HomeIcon />
               </ListItemIcon>
-              <ListItemText classes={{root:classes.address}} primary={`${customers.address}`} />
+              <ListItemText classes={{ root: classes.address }} primary={`${customers.address}`} />
             </ListItem>
             <ListItem>
               <ListItemText primary={`Account Type:  ${customers.account_type}`} />
@@ -115,9 +182,113 @@ export default function Expansion(props) {
         </Box>
         <Collapse in={checked}>
           <Paper elevation={4} className={classes.expansionDropdown}>
-            <svg className={classes.svg}>
-              <polygon points="0,100 50,00, 100,100" className={classes.polygon} />
-            </svg>
+            <Box className={classes.expansionDropdownContent}>
+              <Fade in={activeStep === 0}>
+                <Paper elevation={4}>
+                  <div>
+                    <FormControl className={classes.formControl}>
+                      <InputLabel id="demo-controlled-open-select-label">Bank</InputLabel>
+                      <Select
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        value={bank}
+                        onChange={handleChangeBank}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Lloyds</MenuItem>
+                        <MenuItem value={20}>Halifax</MenuItem>
+                        <MenuItem value={30}>Scotland</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+                  <div>
+                    <FormControl className={classes.formControl}>
+                      <InputLabel id="demo-controlled-open-select-label">Account</InputLabel>
+                      <Select
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        //   open={open}
+                        onClose={handleClose}
+                        onOpen={handleOpen}
+                        value={account}
+                        onChange={handleChangeAccount}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Current</MenuItem>
+                        <MenuItem value={20}>Savings</MenuItem>
+                        <MenuItem value={30}>Investment</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+                </Paper>
+              </Fade>
+              <Fade in={activeStep === 1}>
+                <Paper elevation={4}>
+                  <div>
+                    <FormControl className={classes.formControl}>
+                      <InputLabel id="demo-controlled-open-select-label">Bank</InputLabel>
+                      <Select
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        value={bank}
+                        onChange={handleChangeBank}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Lloyds</MenuItem>
+                        <MenuItem value={20}>Halifax</MenuItem>
+                        <MenuItem value={30}>Scotland</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+                  <div>
+                    <FormControl className={classes.formControl}>
+                      <InputLabel id="demo-controlled-open-select-label">Account</InputLabel>
+                      <Select
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        //   open={open}
+                        onClose={handleClose}
+                        onOpen={handleOpen}
+                        value={account}
+                        onChange={handleChangeAccount}
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Current</MenuItem>
+                        <MenuItem value={20}>Savings</MenuItem>
+                        <MenuItem value={30}>Investment</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+                </Paper>
+              </Fade>
+            </Box>
+            <MobileStepper
+              variant="dots"
+              steps={2}
+              position="static"
+              activeStep={activeStep}
+              className={classes.stepper}
+              nextButton={
+                <Button size="small" onClick={handleNext}>
+                  Next
+                  {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                </Button>
+              }
+              backButton={
+                <Button size="small" onClick={handleBack}>
+                  {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                  Back
+                </Button>
+              }
+            />
           </Paper>
         </Collapse>
         <IconButton classes={{ root: classes.expandIcon }} onClick={onClickBtn}>
