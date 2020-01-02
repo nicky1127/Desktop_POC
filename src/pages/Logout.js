1b822dcdacc2116162c01777b7b8cbc89d9324fa
9503664 <a9503664@9503664s-MacBook-Pro.local>
2020-01-02 12:39:45 +0000

    amend logout page

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import api from '../api/Api';
 
 export default function Logout() {
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    // setTimeout(async () => {
-    //   await initLogout();
-    // }, 200);
+    setTimeout(async () => {
+      await initLogout();
+    }, 200);
   }, []);
 
-  initLogout = async () => {
+  const initLogout = async () => {
     const result = await api.authLogout();
     setReady(true);
+    return result;
   };
+
+  const renderRedirect = () => <Redirect to="/" />;
+
+  const content = ready ? renderRedirect() : 'Logout in progress...';
+
+  return <div>{content}</div>;
 }
