d1b2662fe37336ee54ea5a668314da32d6f43c3e
9503664 <a9503664@9503664s-MacBook-Pro.local>
2020-01-02 12:42:18 +0000

    add logout route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/Login';
-
+import Logout from './pages/Logout';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Login}/>
-        <Route exact path="/page/main" component={Main}/>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/page/main" component={Main} />
+        <Route path="/page/logout" component={Logout} />
       </Switch>
     </Router>
   );
