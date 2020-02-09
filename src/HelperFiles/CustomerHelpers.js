a62f4afa713f1d53923d7bf469ecae39efbc4bb1
9503664 <a9503664@9503664s-mbp.home>
2020-02-09 19:10:39 +0000

    temporary fix in customerRows func

--- src/HelperFiles/CustomerHelpers.js
+++ src/HelperFiles/CustomerHelpers.js
@@ -1,16 +1,17 @@
 export function customerRows(result) {
+  if (!result) result = [];
   const rows = result.map(person => ({
-    party_id:person.party_id,
+    party_id: person.party_id,
     name: person.first_name,
     surname: person.last_name,
     dob: person.date_of_birth,
     sort: person.sort_code,
     address: person.address_line_1,
-    postcode:person.address_postcode
+    postcode: person.address_postcode
   }));
-  console.log(rows)
+  console.log(rows);
 
-  return rows
+  return rows;
 }
 
 export function accountRows(result) {
@@ -19,7 +20,7 @@ export function accountRows(result) {
     account_number: account.account_number,
     sort_code: account.sort_code
   }));
-  console.log(rows)
+  console.log(rows);
 
-  return rows
+  return rows;
 }
